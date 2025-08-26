44c26dd8bda9b332c0dba3581b626da8777b3b13
H.Menuka Sankalpa <menuka.sankalpa@gmail.com>
2025-08-26 12:50:37 +0530

    header.php file updated

---
+++ includes/header.php
@@ -0,0 +1,6 @@
+<div class="header">
+    <form class="search-form">
+        <input type="text" placeholder="Search..." />
+        <button type="submit"><i class="fas fa-search"></i></button>
+    </form>
+</div>
