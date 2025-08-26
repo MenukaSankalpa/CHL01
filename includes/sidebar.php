32e6bfebee6796cafa7fdabb7a0f8fc8bf24712d
H.Menuka Sankalpa <menuka.sankalpa@gmail.com>
2025-08-26 12:50:48 +0530

    sidebar.php updated

---
+++ includes/sidebar.php
@@ -0,0 +1,12 @@
+<div class="sidebar">
+    <div class="logo">Admin Panel</div>
+    <nav>
+        <a href="components/addApplicant.html"><i class="fas fa-user-plus"></i> Add Applicant</a>
+        <a href="components/createUser.php"><i class="fas fa-user-cog"></i> Create Users</a>
+        <a href="components/viewApplicant.php"><i class="fas fa-eye"></i> View Applicant</a>
+        <a href="components/deleteApplicant.php"><i class="fas fa-user-slash"></i> Delete Applicant</a>
+    </nav>
+    <div class="sidebar-logout">
+        <a href="dashboard.php?logout=true" class="sidebar-logout-btn"><i class="fas fa-sign-out-alt"></i> Logout</a>
+    </div>
+</div>
