4705e8a4443e40e4475a8f53768e7e805f529871
H.Menuka Sankalpa <menuka.sankalpa@gmail.com>
2025-08-26 17:14:33 +0530

    header.php file updated

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,11 @@
 <div class="header">
+    <!-- Left Side: Company Logo + Name -->
+    <div class="company-info-header">
+        <img src="/CHL01/images/ceylinegroup.png" alt="Company Logo" class="company-logo-header">
+        <span class="company-label-header">Ceyline Holdings (Pvt) Ltd.</span>
+    </div>
+
+    <!-- Search Bar -->
     <form class="search-form">
         <input type="text" placeholder="Search..." />
         <button type="submit"><i class="fas fa-search"></i></button>
